1d2bd493fe87426cb20b6fb12f411f8188711d37
Pederzh <luigipederzani@gmail.com>
2022-04-20 16:40:26 +0200

    Test amount of the sender and if it is already minted

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,43 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+// describe("Greeter", function () {
+//   it("Should return the new greeting once it's changed", async function () {
+//     const Greeter = await ethers.getContractFactory("Greeter");
+//     const greeter = await Greeter.deploy("Hello, world!");
+//     await greeter.deployed();
+//
+//     expect(await greeter.greet()).to.equal("Hello, world!");
+//
+//     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+//
+//     // wait until the transaction is mined
+//     await setGreetingTx.wait();
+//
+//     expect(await greeter.greet()).to.equal("Hola, mundo!");
+//   });
+// });
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("DerzhNFT", function(){
+  it("Should mint and transfer an NFT to someone", async function(){
+    const DerzhNFT = await ethers.getContractFactory("DerzhNFT");
+    const derzhnft = await DerzhNFT.deploy();
+    await derzhnft.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const recipient = '0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e'; //use public key. To create fake wallets use: npx hardhat node
+    const metadataURI = 'cid/test.png';
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    let balance = await derzhnft.balanceOf(recipient);
+    expect(balance).to.equal(0);
+    const newlyMintedToken = await derzhnft.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther('0.01')});
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    //wait until the transaction is minted
+    await newlyMintedToken.wait();
+
+    balance = await derzhnft.balanceOf(recipient);
+    expect(balance).to.equal(1);
+
+    expect(await derzhnft.isContentOwned(metadataURI)).to.equal(true);
+
   });
 });
