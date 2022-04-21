f0fe1aa89559e5a9dd6808698101feb9ed085fd0
Pederzh <luigipederzani@gmail.com>
2022-04-21 11:41:19 +0200

    Contract filename and contract definition must have the same name in order to be compiled

--- contracts/DerzhNFT.sol
+++ contracts/DerzhNFT.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+//Contract filename and contract definition must have the same name in order to be compiled
 contract DerzhNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
